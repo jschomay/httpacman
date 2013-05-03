149d0d88fb57a49d379581651d95b75294d002b9
jschomay & stevemanuel <stevenhmanuel@gmail.com>
2013-05-02 20:10:16 -0700

    injected our own script and changed regex to detect //cdn style links

--- server/server.js
+++ server/server.js
@@ -12,41 +12,53 @@ server.on('request', function(req, res) {
   // } 
 
 
-  // fs.readFile(__dirname + "/../build" + req.url, function (err,data) {
-  //   if (err) {
-  //     res.writeHead(404);
-  //     res.end(JSON.stringify(err));
-  //     return;
-  //   }
-  //   res.writeHead(200);
-  //   res.end(data);
-  // });
+  
 
-  res.writeHead(200, { 'content-type': 'text/html'});
-  request('http://www.uroulette.com/visit', function(err, response, body) {
-    if(err) {
-      console.log("error:", err.message);
-    } else {
-      host = response.request.uri.host;
+  // /play pulls a random url, all other requests for static content are served in else clase
+  if(req.url === '/play' && req.method === "GET") {
+
+    res.writeHead(200, { 'content-type': 'text/html'});
+    request('http://www.uroulette.com/visit', function(err, response, body) {
+      if(err) {
+        console.log("error:", err.message);
+      } else {
+        host = response.request.uri.host;
 
 
-      // parse for relative paths
-      fixSrcUrls = /src=(["'])(?!http)\/?/gi;
-      fixLinkUrls = /href=(["'])(?!http)\/?/gi;
-      nixScriptTags = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi
-      nixSelfClosingScriptTags = /<script\b.*\/>/gi
-      body = body.replace(fixSrcUrls, 'src=$1http://'+host+'/');
-      body = body.replace(fixLinkUrls, 'href=$1http://'+host+'/');
-      body = body.replace(nixScriptTags, '<!-- script removed by geniuses -->');
-      body = body.replace(nixSelfClosingScriptTags, '<!-- script removed by geniuses -->');
+        // parse for relative paths
+        fixSrcUrls = /src=(["'])(?!http)\/?\/?/gi;
+        fixLinkUrls = /href=(["'])(?!http)\/?\/?/gi;
+        nixScriptTags = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi
+        nixSelfClosingScriptTags = /<script\b.*\/>/gi
+        body = body.replace(fixSrcUrls, 'src=$1http://'+host+'/');
+        body = body.replace(fixLinkUrls, 'href=$1http://'+host+'/');
+        body = body.replace(nixScriptTags, '<!-- script removed by geniuses -->');
+        body = body.replace(nixSelfClosingScriptTags, '<!-- script removed by geniuses -->');
 
+        // put our script in the code
+        headClose = /<\/head>/gi
+        body = body.replace(headClose, '<script src="/js/app.js"></script></head>');
+        
 
-      // write 
-      res.write(body);
-      res.end();
-    }
+        // write 
+        res.write(body);
+        res.end();
+      }
+    });
+  } else {
 
-  });
+    // static content loading
+    fs.readFile(__dirname + "/../build" + req.url, function (err,data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+
+  }
 
 
 });
